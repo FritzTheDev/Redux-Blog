f36aa52c78d73dd8004edaae7a94a8a2727c6494
ottovbismarck <fritzj96@gmail.com>
2018-08-06 20:48:31 -0700

    setting up the component to consume the response.

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,11 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?GarfieldOdie'
+const API_KEY = '?key=GarfieldOdie'
 
 export function fetchPosts() {
-    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+    const request = axios.get(`${ROOT_URL}/posts/${API_KEY}`);
+
     return {
         type: FETCH_POSTS,
         payload: request,
