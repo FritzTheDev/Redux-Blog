86913ed8d3094b714f8450395d47565b1b6eaac9
ottovbismarck <fritzj96@gmail.com>
2018-08-07 03:39:08 -0700

    Added a "Show Post" page and did some redux wiring

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -3,7 +3,6 @@ import { FETCH_POSTS } from '../actions';
 
 export default function(state = {}, action) {
     switch (action.type) {
-
     case FETCH_POSTS:
         return _.mapKeys(action.payload.data, 'id');
     default:
